64708237573be5ad8f22b4af1dd4a8d951629004
Josiah Aubin <josiahaubin@gmail.com>
2019-08-13 15:54:08 -0600

    conditionals js complete

--- app/conditionals.js
+++ app/conditionals.js
@@ -2,7 +2,11 @@
 
 function largerNum(num1, num2) {
     // you code here
-
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
@@ -14,6 +18,18 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
+    let grade = score / possible;
+    if (grade >= .90) {
+        return "A";
+    } else if (grade < .90 && grade >= .80) {
+        return "B";
+    } else if (grade < .80 && grade >= .70) {
+        return "C";
+    } else if (grade < .70 && grade >= .60) {
+        return "D";
+    } else {
+        return "F";
+    }
 
 }
 
@@ -30,12 +46,57 @@ function testGrader(score, possible) {
 
 function timeOfDayGreeting(hour) {
     // you code below
-
+    switch (hour) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            return "Good Night";
+            break;
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+            return "Good Morning";
+            break;
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+            return "Good Afternoon";
+            break;
+        case 18:
+        case 19:
+        case 20:
+        case 21:
+            return "Good Evening";
+            break;
+        case 22:
+        case 23:
+        case 24:
+            return "Good Night";
+            break;
+    }
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
+    let notSickStr = "no fever";
+    let sickStr = "fever";
+    let verySickStr = "go to hospital";
 
+    if (temp <= 98.6) {
+        return notSickStr;
+    } else if (temp > 98.6 && temp < 103) {
+        return sickStr;
+    } else {
+        return sickStr.concat(" ", verySickStr);
+    }
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
@@ -47,7 +108,14 @@ let myCar = {
 }
 
 function isStopped(car) {
-
+    //easy way
+    return !car.moving
+    //longer way
+    // if (car.moving == false) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
@@ -58,5 +126,9 @@ let dish = {
 }
 
 function washDish(dish) {
-
+    if (dish.yourDish == true && dish.isDirty == true) {
+        return true;
+    } else {
+        return false;
+    }
 }
